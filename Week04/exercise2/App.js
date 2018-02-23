c683537a9804639326c833843108b8f77fc055bf
trevh86 <trevor.hurt86@gmail.com>
2018-02-23 10:56:41 +0200

    Adding more exercises

---
+++ Week04/exercise2/App.js
@@ -0,0 +1,84 @@
+import React from 'react';
+import {StyleSheet, Text, TextInput, Alert, View, Button} from 'react-native';
+
+export default class App extends React.Component {
+
+    constructor(props) {
+        super(props);
+        this.state = {text: "Guess a number between 1-100", guesses: 0, number: 0, answer: 0, lowHigh: ""}
+    }
+
+    componentDidMount() {
+        this.randomNumber();
+    };
+
+    // HighOrLow = () => {
+    //     parseInt(this.state.answer) > this.state.number
+    //         ? this.setState({lowHigh: "too high"})
+    //         : this.setState({lowHigh: "too low"})
+    // };
+
+    // guess = () => {
+    //     parseInt(this.state.answer) === this.state.number
+    //         ? (
+    //             this.setState({text: "Correct!"})
+    //         ) : (
+    //             this.HighOrLow(),
+    //             this.setState({text: "Your guess " + this.state.answer + " is " + this.state.lowHigh})
+    //         );
+    //     this.setState((prevState) => {
+    //         return {guesses: prevState.guesses + 1}
+    //     })
+    // };
+
+    guess = () => {
+        this.setState((prevState) => {
+            return {guesses: prevState.guesses + 1}
+        });
+        if (parseInt(this.state.answer) === this.state.number) {
+            this.setState({text: "Correct"});
+            Alert.alert(`You guessed the number in ${this.state.guesses} guesses`, "")
+        } else if (parseInt(this.state.answer) > this.state.number) {
+            this.setState({text: `Your guess ${this.state.answer} is too high`})
+        } else if (parseInt(this.state.answer) < this.state.number) {
+            this.setState({text: `Your guess ${this.state.answer} is too low`})
+        } else {
+            this.setState({text: "Invalid guess"})
+        }
+    };
+
+    randomNumber = () => {
+        this.setState(() => {
+            return {number: Math.floor(Math.random() * 100 + 1)}
+        })
+    };
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.text}>{this.state.text}</Text>
+                <Button title="MAKE GUESS" onPress={this.guess}/>
+                <TextInput style={styles.textInput} keyboardType={"phone-pad"}
+                           onChangeText={(answer) => this.setState({answer})}/>
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInput: {
+        width: 200,
+        borderColor: '#fff',
+        borderWidth: 1,
+        color: 'white',
+    },
+    text: {
+        color: '#fff'
+    },
+});
