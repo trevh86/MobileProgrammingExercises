e415f46bc1fc67d6358b4d1588031b8e0072c4b1
trevh86 <trevor.hurt86@gmail.com>
2018-03-03 16:07:36 +0200

    Finished exercise 7

--- Week04/exercise7/App.js
+++ Week04/exercise7/App.js
@@ -1,23 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View, TextInput, Button, FlatList, Alert, StatusBar, Image} from 'react-native';
 
 export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Changes you make will automatically reload.</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-      </View>
-    );
-  }
+    constructor(props) {
+        super(props);
+        this.state = {searchTerm: '', recipeList: []};
+    }
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <StatusBar hidden={true}/>
+                <FlatList data={this.state.recipeList}
+                          keyExtractor={item => item.href}
+                          renderItem={({item}) =>
+                              <View>
+                                  <Text style={styles.text} >{item.title}</Text>
+                                  <Image style={{width: 50, height: 50}} source={{uri: item.thumbnail}}/>
+                              </View>
+                          }
+                          ItemSeperatorComponent={this.listSeparator}/>
+                <Text style={styles.text}>Enter ingredient:</Text>
+                <TextInput style={styles.textInput} placeholder=' Ingredient'
+                           onChangeText={(searchTerm) => this.setState({searchTerm})}/>
+                <Button title="SEARCH" onPress={this.search}/>
+            </View>
+        );
+    }
+
+    search = () => {
+        const url = 'http://www.recipepuppy.com/api/?i=' + this.state.searchTerm;
+        fetch(url)
+            .then((response) => response.json())
+            .then((responseJson) => {
+                this.setState({recipeList: responseJson.results});
+            })
+            .catch((error) => {
+                Alert.alert(error);
+            })
+    };
+
+    listSeparator = () => {
+        return (
+            <View
+                style={{
+                    height: 1,
+                    width: "80%",
+                    backgroundColor: "#CED0CE",
+                    marginLeft: "10%"
+                }}
+            />
+        );
+    };
+
 }
 
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
+    container: {
+        flex: 1,
+        backgroundColor: '#000',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+    textInput: {
+        width: 200,
+        borderColor: '#fff',
+        borderWidth: 1,
+        color: 'white',
+    },
+    text: {
+        color: '#fff'
+    },
 });
